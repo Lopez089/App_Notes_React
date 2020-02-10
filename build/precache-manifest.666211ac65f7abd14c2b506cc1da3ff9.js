f75c159341ec95aaba3c8ec8b7088039a8f0d138
Juan Lopez <contacto@juanlopezaragon.es>
2020-02-10 15:06:34 +0100

    buil

---
+++ build/precache-manifest.666211ac65f7abd14c2b506cc1da3ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d13e1fa856c74030d7dda5c1dabe70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e2d50fc6b644491cb4a",
+    "url": "/static/css/main.3b8205d8.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "4e2d50fc6b644491cb4a",
+    "url": "/static/js/main.9e6973e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
